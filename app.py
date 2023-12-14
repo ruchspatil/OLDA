da31c8fdf78dd6bcde42a8e89d5af09b689e2a26
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2023-12-14 14:54:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,10 +82,6 @@ if user_menu == 'OVERALL ANALYSIS':
         st.header("Athletes")
         st.title(athletes)
 
-    nations_over_time = helper.data_over_time(df,'region')
-    fig = px.line(nations_over_time, x="Edition", y="region")
-    st.title("Participating Nations over the years")
-    st.plotly_chart(fig)
 
     events_over_time = helper.data_over_time(df, 'Event')
     fig = px.line(events_over_time, x="Edition", y="Event")
