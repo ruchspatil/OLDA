a50476643571904fe8d19984ba59e0c1a57dd95f
Ruchita Patil <71245429+ruchspatil@users.noreply.github.com>
2023-12-14 15:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,11 +149,5 @@ if user_menu == 'ATHLETE WISE ANALYSIS':
   
 
 
-    st.title("Men Vs Women Participation Over the Years")
-    final = helper.men_vs_women(df)
-    fig = px.line(final, x="Year", y=["Male", "Female"])
-    fig.update_layout(autosize=False, width=1000, height=600)
-    st.plotly_chart(fig)
-
 
 
